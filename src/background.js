effa188562db9bb04e5f4b90d9e1a1d644ea674c
Michael Mano <michael.mano26@gmail.com>
2021-03-09 14:10:19 +1000

    Add store and store example

--- src/background.js
+++ src/background.js
@@ -3,11 +3,13 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import store from './store'
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
+  { scheme: 'app', privileges: { secure: true, standard: true } },
 ])
 
 async function createWindow() {
@@ -16,11 +18,10 @@ async function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-    }
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+    },
   })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
@@ -78,3 +79,7 @@ if (isDevelopment) {
     })
   }
 }
+
+store.subscribe((mutation) => {
+  console.log(mutation)
+})
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import { createPersistedState, createSharedMutations } from 'vuex-electron';
 
 export default createStore({
   state: {
+    count: 0,
   },
   mutations: {
+    COMMIT_INCREMENT: (state) => {
+      state.count = state.count++;
+    },
+  },
+  getters: {
+    count: (state) => state.count,
   },
   actions: {
+    INCREMENT: ({ commit }) => commit('COMMIT_INCREMENT'),
   },
-  modules: {
-  }
-})
+  modules: {},
+  plugins: [
+    createPersistedState({
+      storageName: 'keeloc',
+    }),
+    createSharedMutations(),
+  ],
+});
