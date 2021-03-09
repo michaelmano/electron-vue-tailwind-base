af7b806f3b63ff6b88ae59261ccd4d274469b385
Michael Mano <michael.mano26@gmail.com>
2021-03-09 13:49:07 +1000

    Add scss to the project

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './tailwind.css'
+import './styles/index.scss'
 
 createApp(App).use(store).use(router).mount('#app')
