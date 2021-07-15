21c04452f4a236912ace472bc95a824c2deab09a
Michael Mano <michael.mano26@gmail.com>
2021-07-16 08:59:44 +1000

    update

--- src/background.js
+++ src/background.js
@@ -15,8 +15,8 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    minWidth: 800,
+    minHeight: 610,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
@@ -35,6 +35,8 @@ async function createWindow() {
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+
+  win.maximize()
 }
 
 // Quit when all windows are closed.
