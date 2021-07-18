e59422de80790addbbfca9601e56992d137f98f5
Michael Mano <michael.mano26@gmail.com>
2021-07-19 09:00:30 +1000

    Add resizable sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="title-bar" @dblclick="toggleTitleBar"></div>
-  <router-view/>
+  <div class="flex">
+    <aside class="sidebar" ref="sidebar">
+      <div class="sidebar__resizer" @mousedown="initResize"></div>
+      <ul>
+        <li>Sidebar item</li>
+        <li>Sidebar item</li>
+        <li>Sidebar item</li>
+        <li>Sidebar item</li>
+        <li>Sidebar item</li>
+        <li>Sidebar item</li>
+      </ul>
+    </aside>
+    <div class="w-auto w-full">
+      <router-view/>
+    </div>
+  </div>
 </template>
 <script>
 const { ipcRenderer } = require('electron');
@@ -9,7 +24,18 @@ export default {
   methods: {
     toggleTitleBar() {
       ipcRenderer.send('toggle-window-size');
-    }
+    },
+    initResize() {
+      window.addEventListener('mousemove', this.resize, false);
+      window.addEventListener('mouseup', this.stopResize, false);
+    },
+    resize(event) {
+      this.$refs.sidebar.style.width = event.clientX + 'px';
+    },
+    stopResize() {
+      window.removeEventListener('mousemove', this.resize, false);
+      window.removeEventListener('mouseup', this.stopResize, false);
+    },
   },
 };
 </script>
